98f9baf878b561095de4fbbcee9cae63298fa965
Anuj A <anujamirthalingam5@gmail.com>
2024-07-11 22:16:31 +0530

    Added init method

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 class Item:
-    def calc_price(self, a, b):
-        return a*b
+    discount_rate = 0.8
+
+    def __init__(self, name, price, quantity):
+        assert price >= 0, "Price must be greater than or equal to zero"
+        assert quantity >= 0, "Quantity must be greater than or equal to zero"
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calc_price(self):
+        return self.price * self.quantity
+
+    def calc_discount(self):
+        self.price = self.price * Item.discount_rate
 
 
-item1 = Item()
-item1.name = "Pencil"
-item1.price = 50
-item1.quantity = 10
-item1.calc_price(item1.price, item1.quantity)
-print("The total amount is ", item1.calc_price(item1.price, item1.quantity))
+item1 = Item("Pen", 30, 10)
+item1.calc_discount()
+print("The total discount amount is ", item1.price)
+print("The total price is ", item1.calc_price())
